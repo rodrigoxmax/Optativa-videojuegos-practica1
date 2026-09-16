0e258d41fef5b674204ad335eb43adbf045fd751
rodrigoxmax <rodrigo.palomobautista@gmail.com>
2026-09-16 17:11:59 +0200

    purcharse ycustomer

--- src/projecto1/Customer.java
+++ src/projecto1/Customer.java
@@ -1,5 +1,48 @@
 package projecto1;
 
-public class Customer {
+public class Customer extends Exception {
+	private int id_customer;
+	private String name;
+	private double balance;
+public Customer ( String name, double balance) {
+	this.name=name;
+	this.balance=balance;
+}
+public int getid_costumer() {
+	return id_customer;
+}
+public String  getnombre() {
+	return nombre;
+}
+public double getbalace() {
+	return id_customer;
+}
+public void añadirb (double d) {
+	 this.balance+añadirb;
+}
+public void retirarbalance(double balanceresto) {
+	
+		try {
+			this.balance-=balanceresto;
+		}catch(Exception bloque) {
+			System.out.println("Error no puedes hacer eso");
+			
+		}
+	}else {
+		this.balance-=balanceresto;
+	}
+}
+public void comprobar (double precio) throws Exception {
+	
+		
+	if(saldo<precio) {
+		throws new Exception("no puedes comprar con el saldo actual que tienes ");
+	}
+	
+}
+public String toString() {
+	return "Nombre"+nombre+"saldo"+balance;
+	
+}
 
 }
--- src/projecto1/Purchase.java
+++ src/projecto1/Purchase.java
@@ -1,5 +1,32 @@
 package projecto1;
 
 public class Purchase {
+	private Customer customer;
+	private Game game;
+	private int quantity;
+	private double preciofianl;
+	
+public Purcharse(Customer customer,Game game,int quantity,double preciofianl) {
+	this.customer=customer;
+	this.game=game;
+	this.quantity=quantity;
+	this.preciofianl=quantity*game.getprecio();
+	
+}
+public Customer getclient() {
+	return customer;
+	
+}
+public game getgame() {
+	return customer;
+	
+}
+public int getquantity() {
+	return quantity;
+	
+}
+public double getpreciofinal () {
+	return preciofianl;
+}
 
 }
